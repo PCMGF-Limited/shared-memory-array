143f494673507c52f47c1b44926d7bc13bf00cad
fra <francesco.strino@pcmgf.com>
2026-02-13 21:25:05 +0200

    updated _version

--- shared_memory_array/__init__.py
+++ shared_memory_array/__init__.py
@@ -1,1 +1,7 @@
 from .shared_memory_array import SharedMemoryArray, ManagedSharedMemoryArray
+
+# Get the version from _version.py (added when building using scm)
+try:
+    from ._version import __version__ # noqa
+except ModuleNotFoundError as e:
+    __version__ = '0.0.0-dev'
